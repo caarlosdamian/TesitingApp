2eb1753fa197454e89131a7aa0c20fc4d33da80e
caarlosdamian <dapecarlos@gmail.com>
2021-06-05 17:02:39 -0500

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Logo from "./assets/Logo.png";
+import UserIcon from "./assets/user-icon.png";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <head className="App-header">
+        <title>Header</title>
+        <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200;1,500&display=swap" rel="stylesheet"></link>
+      </head>
+      <body>
+        <header className="header">
+          <img src={Logo} alt="logo" className="header_img"></img>
+          <div className="header_menu">
+            <div className="header_menu--profile">
+              <img src={UserIcon} alt="Perfil"></img>
+              <p>Perfil</p>
+            </div>
+            <ul>
+              <li>
+                <a href="/">Cuenta</a>
+              </li>
+              <li>
+                <a href="/">Cerrar Sesion</a>
+              </li>
+            </ul>
+          </div>
+        </header>
+      </body>
     </div>
   );
 }
